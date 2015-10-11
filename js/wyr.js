fa8515e7af12e082e0d4d1daa02d4201b437d596
Lindsay Goff <lindsayagoff@gmail.com>
2015-10-10 21:51:42 -0500

    Styled, all buttons showing

---
+++ js/wyr.js
@@ -0,0 +1,27 @@
+
+
+
+
+// LAST QUESTION 
+
+// function endResponse(){
+// 		// //version3
+// 	if (humanMsg.indexOf("hogwarts") !== -1){
+// 		computerMsg = "You&#39;re my kind of person, lets be friends!";
+// 		human.className="displayNone";
+// 		button.className="displayNone";
+// 		setTimeout(endGame,1200);
+// 	}else if (humanMsg.indexOf("narnia") !== -1){
+// 		computerMsg = "Wrong answer, see you later!";
+// 		human.className="displayNone";
+// 		button.className="displayNone";
+// 		setTimeout(endGame,1200);
+// 	}
+// }
+
+// function endGame(){
+// 	////.assign() leaves the back button working
+// 	////.replace() disables the back button by replacing the location in the history
+// 	location.assign("http://buzzfeed.com/danieldalton/griffin-door#.yeRolMaaea");
+// }
+
